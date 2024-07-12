fa447ae483c9d9a1e9381391f331bcb644a2045f
prezi <mistepien@wp.pl>
2024-07-12 11:44:24 +0200

    hardware XOR
    Hardware XOR is added thus library is more efficient.

--- firmware/SegaController.cpp
+++ firmware/SegaController.cpp
@@ -1,5 +1,4 @@
-//
-// SegaController.h (with port registers and faster reading loop)
+// SegaController.h (with port registers, faster reading loop and lots of optimalization)
 //
 // Author:
 //       Jon Thysell <thysell@gmail.com>
@@ -26,6 +25,38 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+/*----------------------------------------
+  |   DDRx   |   PORTx  |    result      |
+  ---------------------------------------|
+  ---------------------------------------|
+  |    0     |     0    |     INPUT      |
+  ----------------------------------------
+  |    0     |     1    |  INPUT_PULLUP  |
+  ----------------------------------------
+  |    1     |     0    | OUTPUT (LOW)   |
+  ----------------------------------------
+  |    1     |     1    | OUTPUT (HIGH)  |
+  ----------------------------------------
+
+  LOW(0) state of both registers is DEFAULT,
+  thus every pin is in INPUT mode without doing anything.
+
+  ------------------            -----------------
+  |  HARDWARE XOR  |            |  SOFTWARE XOR |
+  ------------------            -----------------
+  PINx = byte;    <========>    PORTx ^= byte;
+
+  ------------------
+  |  HARDWARE XOR  |
+  ------------------
+
+  "The port input pins I/O location is read only, while the data register and the
+  data direction register are read/write. However, writing a logic one to a bit
+  in the PINx register, will result in a toggle in the
+  corresponding bit in the data register."
+
+  That is more efficient since XOR operation is done in hardware, not software,
+  and it saves cycles since in code there is no need to bother about XOR.*/
 
 #include "Arduino.h"
 #include "SegaController.h"
@@ -40,184 +71,176 @@ SegaController::SegaController(byte db9_pin_7, byte db9_pin_1, byte db9_pin_2, b
   _inputPins[4] = db9_pin_6;
   _inputPins[5] = db9_pin_9;
 
+  bitSet(DDR_REG_select_Pin, _selectPin);  // Setup selectPin as OUTPUT
+  //bitClear(PORT_REG_selectPin, _selectPin); // Setup selectPin -- default LOW is ok
+
+  /* Setup input pins
+
+  Default state of PINS is INPUT -- without INPUT_PULLUP
+  INPUT_PULLUP was not enough reliable in case of ATMEGA48PA
+  For MDJOY PCB 10K Ohm pull-up resistors are ok.
+  Thus _inputPin[x] are in INPUT (not INPUT_PULLUP) by default
+  */
+
+#if defined(ARDUINO_AVR_MICRO)
   byte _all_inputPins = 0;
   for (byte i = 0; i < SC_INPUT_PINS; i++) {
-    _all_inputPins |= bit( _inputPins[i]);
+    _all_inputPins |= bit(_inputPins[i]);
   }
-
-  bitSet(DDR_REG_select_Pin, _selectPin); // Setup selectPin as OUTPUT
-  bitSet(PORT_REG_selectPin, _selectPin); // Setup selectPin as HIGH
-
-  // Setup input pins
-#if defined(ARDUINO_ARCH_AVR)
-  /*  bitSet(PORT_REG_inputPins,_inputPins[i]); (INPUT_PULLUP) ===> internal PULL-UP
-  */
-  DDR_REG_inputPins &=  ~_all_inputPins;
-  PORT_REG_inputPins |=   _all_inputPins;
-
-#else
-  /* bitClear(PORT_REG_inputPins,_inputPins[i]); (INPUT, not INPUT_PULLUP)  --> external PULL-UP
-     INPUT_PULLUP was not enough reliable in case of ATMEGA48PA
-     For MDJOY PCB 10K Ohm pull-up resistors are ok.
-  */
-  DDR_REG_inputPins &=  ~_all_inputPins;
-  PORT_REG_inputPins &= ~_all_inputPins;
+  //DDR_REG_inputPins &= ~_all_inputPins; //INPUT by default
+  PORT_REG_inputPins |= _all_inputPins;
 #endif
-
-  _currentState = 0;
-  _sixButtonMode = false;
-  _lastReadTime = 0;
 }
 
 word SegaController::getState() {
+  static word _currentState;
+  static unsigned long _lastReadTime;
   if ((millis() - _lastReadTime) < SC_READ_DELAY_MS) {
     // Not enough time has elapsed, return previously read state
     return _currentState;
   }
 
-  // Clear current state
-  _currentState = 0;
-  byte _readCycle_regs[SC_CYCLES_6BTN];
 
   //changing number of cycles depending is it 3BTN-mode or 6BTN-mode
   byte SC_CYCLES = SC_CYCLES_3BTN;
-  if (_sixButtonMode) {
+  if (bitRead(_currentState, SC_MODE)) {  //_currentState is not yet cleared
     SC_CYCLES = SC_CYCLES_6BTN;
   }
 
+  byte _readCycle_regs[8];
+  byte _selectPin_bin = bit(_selectPin);
+
   noInterrupts();
 
-  //This is here the most time sensitive piece of code
-  //slowness means failure in communication  with gamepad
-  //SEGA gamepad readings of port registers are dumped to the byte array
-  //It works like a kind of shiftIn -- but output is not a byte but
-  //an array of bytes
-  //That's all folks
+  /*Here is the most time sensitive piece of code.
+  Being slow means failure in communication  with gamepad.
+  SEGA gamepad readings of port registers are dumped to the byte array.
+  It works like a kind of shiftIn -- but output is not a byte but
+  an array of bytes.
 
-  for (byte cycle = 0; cycle < SC_CYCLES; cycle++) {
-    _readCycle_regs[cycle] = readCycle(cycle);
+  That's all folks!
+
+  BTW, from technical point of view Sega Genesis 6button game pad
+  protocol is just a piece of crap. Expansion from 3button to 6button
+  made it very dependent upon very accurate delay :D
+  For instance SNES protocol is much better.
+  */
+#pragma GCC unroll 8
+  for (byte cycle = 0; cycle < 8; cycle++) {
+    _delay_us(SC_CYCLE_DELAY_US);
+    _readCycle_regs[cycle] = PIN_REG_inputPins;
+    PIN_REG_selectPin = _selectPin_bin;  //hardware XOR
   }
 
   interrupts();
 
-  //Here slowness is not an issue any more since reading of Sega gamepad is over
-  //What happens is processing of already collected data and producing output
-  for (byte cycle = 0; cycle < SC_CYCLES - 2; cycle++) {
-    writeCycle_regs_to_state(cycle + 2, _readCycle_regs[cycle + 2]);
+  //Here slowness is not an issue any more since reading of Sega gamepad is already done.
+  //What happens is processing of already collected data and producing output.
+
+  _currentState = 0;  // Clear current state
+  for (byte cycle = 0; cycle < SC_CYCLES; cycle++) {
+    _currentState |= writeCycle_regs_to_state(cycle, _readCycle_regs[cycle]);
+    if (1 ^ bitRead(_currentState, SC_CTL_ON)) {
+      break;
+      /* When a controller disconnects, revert to three-button polling
+        and stop proccessing controller output.
+        Since SC_CTL_ON is pushed into _currentState in cycle=0
+        then _currentState=0 is if (cycle=0 and SC_CTL_ON=0).
+        _currentState is  cleared just before the for loop.
+      */
+    }
   }
   _lastReadTime = millis();
 
   return _currentState;
 }
 
-byte SegaController::readCycle(byte cycle) {
-
-  // Set the select pin low/high
-  bitWrite(PORT_REG_selectPin, _selectPin, cycle & 1);
-
-  byte SEGA_reg = 0;
-
+word SegaController::writeCycle_regs_to_state(byte cycle, byte SEGAinput) {
+  word _cycle_state = 0;
+  SEGAinput = ~(SEGAinput);  //switch from logic {LOW=pressed, HIGH=released} to logic {HIGH=pressed, LOW=released}
   switch (cycle) {
     case 0:
-    case 1:
+      // Check that a controller is connected // getState() imidiatelly returns 0 and does not read other cases
+      if ((bitRead(SEGAinput, _inputPins[2])) && (bitRead(SEGAinput, _inputPins[3]))) {
+        bitSet(_cycle_state, SC_CTL_ON);
+      }
       break;
-    default :
-      //"stabilizing" gamepad
-      delayMicroseconds(SC_CYCLE_DELAY_US);
-      SEGA_reg = PIN_REG_inputPins;
-  }
-
-  return SEGA_reg;
-}
-
-void SegaController::writeCycle_regs_to_state(byte cycle, byte SEGAinput) {
-  SEGAinput = ~(SEGAinput); //switch from logic {LOW=pressed, HIGH=released} to logic {HIGH=pressed, LOW=released}
-  switch (cycle) {
     case 2:
-      // Check that a controller is connected
-      if ((bitRead(SEGAinput, _inputPins[2]) && bitRead(SEGAinput, _inputPins[3]))) {
-        bitSet(_currentState, SC_CTL_ON);
-        // Read input pins for A, Start
-        if (bitRead(SEGAinput, _inputPins[4])) {
-          bitSet(_currentState, SC_BTN_A);
-        }
-        if (bitRead(SEGAinput, _inputPins[5])) {
-          bitSet(_currentState, SC_BTN_START);
-        }
+      // Read input pins for A, Start
+      if (bitRead(SEGAinput, _inputPins[4])) {
+        bitSet(_cycle_state, SC_BTN_A);
+      }
+      if (bitRead(SEGAinput, _inputPins[5])) {
+        bitSet(_cycle_state, SC_BTN_START);
       }
       break;
-    case 3:
-      if (bitRead(_currentState, SC_CTL_ON)) {
-        // Read input pins for Up, Down, Left, Right, B, C
-        if (bitRead(SEGAinput, _inputPins[0])) {
-          bitSet(_currentState, SC_BTN_UP);
-        }
-        if (bitRead(SEGAinput, _inputPins[1])) {
-          bitSet(_currentState, SC_BTN_DOWN);
-        }
-        if (bitRead(SEGAinput, _inputPins[2])) {
-          bitSet(_currentState, SC_BTN_LEFT);
-        }
-        if (bitRead(SEGAinput, _inputPins[3])) {
-          bitSet(_currentState, SC_BTN_RIGHT);
-        }
-        if (bitRead(SEGAinput, _inputPins[4])) {
-          bitSet(_currentState, SC_BTN_B);
-        }
-        if (bitRead(SEGAinput, _inputPins[5])) {
-          bitSet(_currentState, SC_BTN_C);
-        }
-
-        //NO OPPOSITE DIRECTIONS
-        bool SC_BTN_UP_tmp = nod(bitRead(_currentState, SC_BTN_UP), bitRead(_currentState, SC_BTN_DOWN));
-        bool SC_BTN_DOWN_tmp = nod(bitRead(_currentState, SC_BTN_DOWN), bitRead(_currentState, SC_BTN_UP));
-        bool SC_BTN_LEFT_tmp = nod(bitRead(_currentState, SC_BTN_LEFT), bitRead(_currentState, SC_BTN_RIGHT));
-        bool SC_BTN_RIGHT_tmp = nod(bitRead(_currentState, SC_BTN_RIGHT), bitRead(_currentState, SC_BTN_LEFT));
-        bitWrite(_currentState, SC_BTN_UP, SC_BTN_UP_tmp);
-        bitWrite(_currentState, SC_BTN_DOWN, SC_BTN_DOWN_tmp);
-        bitWrite(_currentState, SC_BTN_LEFT, SC_BTN_LEFT_tmp);
-        bitWrite(_currentState, SC_BTN_RIGHT, SC_BTN_RIGHT_tmp);
+    case 1:  //it can be also 3 -- the same result
+      // Read input pins for Up, Down, Left, Right, B, C
+      if (bitRead(SEGAinput, _inputPins[0])) {
+        bitSet(_cycle_state, SC_DPAD_UP);
       }
+      if (bitRead(SEGAinput, _inputPins[1])) {
+        bitSet(_cycle_state, SC_DPAD_DOWN);
+      }
+      if (bitRead(SEGAinput, _inputPins[2])) {
+        bitSet(_cycle_state, SC_DPAD_LEFT);
+      }
+      if (bitRead(SEGAinput, _inputPins[3])) {
+        bitSet(_cycle_state, SC_DPAD_RIGHT);
+      }
+      if (bitRead(SEGAinput, _inputPins[4])) {
+        bitSet(_cycle_state, SC_BTN_B);
+      }
+      if (bitRead(SEGAinput, _inputPins[5])) {
+        bitSet(_cycle_state, SC_BTN_C);
+      }
+      _cycle_state = nod_dpad(_cycle_state);  //NO OPPOSITE DIRECTIONS
       break;
     case 4:
-      if (bitRead(_currentState, SC_CTL_ON)) {  // When a controller disconnects, revert to three-button polling
-        _sixButtonMode = (bitRead(SEGAinput, _inputPins[0]) && bitRead(SEGAinput, _inputPins[1]));
-      } else {
-        _sixButtonMode = false;
+      if (bitRead(SEGAinput, _inputPins[0]) && bitRead(SEGAinput, _inputPins[1])) {
+        bitSet(_cycle_state, SC_MODE);
       }
       break;
     case 5:
-      if (_sixButtonMode) {
-        bitSet(_currentState, SC_MODE);
-        // Read input pins for X, Y, Z, Mode
-        if (bitRead(SEGAinput, _inputPins[0])) {
-          bitSet(_currentState, SC_BTN_Z);
-        }
-        if (bitRead(SEGAinput, _inputPins[1])) {
-          bitSet(_currentState, SC_BTN_Y);
-        }
-        if (bitRead(SEGAinput, _inputPins[2])) {
-          bitSet(_currentState, SC_BTN_X);
-        }
-        /*if (bitRead(SEGAinput, _inputPins[3])) {
-           bitSet(_currentState, SC_BTN_MODE);
-          }
-        */
+      // Read input pins for X, Y, Z, Mode
+      if (bitRead(SEGAinput, _inputPins[0])) {
+        bitSet(_cycle_state, SC_BTN_Z);
       }
+      if (bitRead(SEGAinput, _inputPins[1])) {
+        bitSet(_cycle_state, SC_BTN_Y);
+      }
+      if (bitRead(SEGAinput, _inputPins[2])) {
+        bitSet(_cycle_state, SC_BTN_X);
+      }
+      /*if (bitRead(SEGAinput, _inputPins[3])) {
+         bitSet(_currentState, SC_BTN_MODE);
+        }
+      */
       break;
     case 6:
-      if (_sixButtonMode) {
-        // Read input pins for HOME button -- that is a feature of 8Bitdo M30 2.4
-        if (bitRead(SEGAinput, _inputPins[0])) {
-          bitSet(_currentState, SC_BTN_HOME);
-        }
+      // Read input pins for HOME button -- that is a feature of 8Bitdo M30 2.4
+      if (bitRead(SEGAinput, _inputPins[0])) {
+        bitSet(_cycle_state, SC_BTN_HOME);
       }
       break;
   }
+  return _cycle_state;
+}
+
+word SegaController::nod_dpad(word _gamepadState) {
+  bool SC_DPAD_UP_tmp = nod(bitRead(_gamepadState, SC_DPAD_UP), bitRead(_gamepadState, SC_DPAD_DOWN));
+  bool SC_DPAD_DOWN_tmp = nod(bitRead(_gamepadState, SC_DPAD_DOWN), bitRead(_gamepadState, SC_DPAD_UP));
+  bool SC_DPAD_LEFT_tmp = nod(bitRead(_gamepadState, SC_DPAD_LEFT), bitRead(_gamepadState, SC_DPAD_RIGHT));
+  bool SC_DPAD_RIGHT_tmp = nod(bitRead(_gamepadState, SC_DPAD_RIGHT), bitRead(_gamepadState, SC_DPAD_LEFT));
+  bitWrite(_gamepadState, SC_DPAD_UP, SC_DPAD_UP_tmp);
+  bitWrite(_gamepadState, SC_DPAD_DOWN, SC_DPAD_DOWN_tmp);
+  bitWrite(_gamepadState, SC_DPAD_LEFT, SC_DPAD_LEFT_tmp);
+  bitWrite(_gamepadState, SC_DPAD_RIGHT, SC_DPAD_RIGHT_tmp);
+  return _gamepadState;
 }
 
 bool SegaController::nod(bool OUT_DIR, bool SECOND_DIR) {
   //bool value = ((!( OUT_DIR  || SECOND_DIR )) || OUT_DIR); //logic where LOW=pressed, HIGH=released
-  bool value = ((!( OUT_DIR  && SECOND_DIR )) && OUT_DIR); //logic where HIGH=pressed, LOW=released
+  bool value = ((!(OUT_DIR && SECOND_DIR)) && OUT_DIR);  //logic where HIGH=pressed, LOW=released
   return value;
 }
--- firmware/SegaController.h
+++ firmware/SegaController.h
@@ -1,10 +1,13 @@
+#pragma GCC optimize ("Ofast,unroll-loops,inline")
 //
-// SegaController.h (with port registers by mistepien@wp.pl)
+// SegaController.h (with port registers and faster reading loop)
 //
 // Author:
 //       Jon Thysell <thysell@gmail.com>
+//       Michał Stępień <mistepien@wp.pl>
 //
 // Copyright (c) 2017 Jon Thysell <http://jonthysell.com>
+// Copyright (c) 2023 Michał Stępień <mistepien@wp.pl>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -28,46 +31,72 @@ enum sega_state {
   SC_CTL_ON = 0,  // The controller is connected
   SC_MODE = 1,    //checking six or three mode 1
   SC_BTN_START = 2,
-  SC_BTN_UP = 3,
-  SC_BTN_DOWN = 4,
-  SC_BTN_LEFT = 5,
-  SC_BTN_RIGHT = 6,
+  SC_DPAD_UP = 3,
+  SC_DPAD_DOWN = 4,
+  SC_DPAD_LEFT = 5,
+  SC_DPAD_RIGHT = 6,
   SC_BTN_A = 7,
   SC_BTN_B = 8,
   SC_BTN_C = 9,
   SC_BTN_X = 10,
   SC_BTN_Y = 11,
   SC_BTN_Z = 12,
-  SC_BTN_HOME = 13 //availabe in M30 2.4 8Bitdo gamepad
-  //SC_BTN_MODE = 14
+  SC_BTN_HOME = 13
+                /*SC_BTN_MODE = 14, //availabe in M30 2.4 8Bitdo gamepad
+                */
 };
+
+
+#if defined(ARDUINO_AVR_MICRO)
 
 #define DDR_REG_select_Pin DDRD
 #define PORT_REG_selectPin PORTD
+#define PIN_REG_selectPin PIND
 
 #define DDR_REG_inputPins DDRB
 #define PORT_REG_inputPins PORTB
 #define PIN_REG_inputPins PINB
 
+#else
+
+#define DDR_REG_select_Pin DDRD
+#define PORT_REG_selectPin PORTD
+#define PIN_REG_selectPin PIND
+
+#define DDR_REG_inputPins DDRD
+#define PORT_REG_inputPins PORTD
+#define PIN_REG_inputPins PIND
+
+#endif
+
+
 constexpr byte SC_INPUT_PINS = 6;
-constexpr byte SC_CYCLES_6BTN = 7; 
+constexpr byte SC_CYCLES_6BTN = 7;
 constexpr byte SC_CYCLES_3BTN = 5;
-constexpr byte SC_READ_DELAY_MS = 4;  // Must be >= 3ms to give 6-button controller time to reset
-constexpr byte SC_CYCLE_DELAY_US = 6;  // Delay (µs) between setting the select pin and reading the button pins
+constexpr unsigned long SC_READ_DELAY_MS = 4;  // Must be >= 3ms to give 6-button controller time to reset
+
+// Delay (µs) between setting the select pin and reading the button pins
+#if defined(ARDUINO_AVR_MICRO)
+constexpr unsigned long SC_CYCLE_DELAY_US = 6;
+#elif (F_CPU == 3686400L) || (F_CPU == 3276800L) || (F_CPU == 2000000L) || (F_CPU == 1000000L)
+constexpr unsigned long SC_CYCLE_DELAY_US = 2;
+#elif (F_CPU == 4000000L)
+constexpr unsigned long SC_CYCLE_DELAY_US = 4; //4MHz: 0..5 -- retrobit,   0..20 - 8bido M30 2.4, 0..100 - chinesse no-name
+#elif (F_CPU == 6000000L) || (F_CPU == 7372800L) || (F_CPU == 8000000L)
+constexpr unsigned long SC_CYCLE_DELAY_US = 7; //6MHz: 4..11 --retrobit, 7.3728MHz: 0..15 -- retrobit, 8MHZ: 0..13 -- retrobit
+#elif (F_CPU == 11059200L) || (F_CPU == 16000000L) || (F_CPU == 12000000L) || (F_CPU == 14745600)
+constexpr unsigned long SC_CYCLE_DELAY_US = 10; //11.0592MHz: 3..18 -- retrobit, 16MHz: 0..17 -- retrobit
+#endif
 
 class SegaController {
-public:
-  SegaController(byte db9_pin_7, byte db9_pin_1, byte db9_pin_2, byte db9_pin_3, byte db9_pin_4, byte db9_pin_6, byte db9_pin_9);
-  word getState();
-private:
-  byte readCycle(byte cycle);
-  void writeCycle_regs_to_state(byte cycle, byte SEGA_reg);
-  word _currentState;
-  unsigned long _lastReadTime;
-  boolean _sixButtonMode;
-  byte _selectPin;  // output select pin
-  byte _inputPins[SC_INPUT_PINS];
-  bool nod(bool KEY_DIR, bool SECONDARY_DIR);
+  public:
+    SegaController(byte db9_pin_7, byte db9_pin_1, byte db9_pin_2, byte db9_pin_3, byte db9_pin_4, byte db9_pin_6, byte db9_pin_9);
+    word getState();
+    bool nod(bool KEY_DIR, bool SECONDARY_DIR);
+  private:
+    word writeCycle_regs_to_state(byte cycle, byte SEGA_reg);
+    word nod_dpad(word _gamepadState);
+    byte _selectPin;  // output select pin
+    byte _inputPins[SC_INPUT_PINS];
 };
-
-//#endif
+//#pragma GCC pop_options
